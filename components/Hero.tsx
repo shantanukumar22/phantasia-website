fe3ec1f3d915af5aa0b6953a2e1ffd66faa9f098
shantanukumar22 <justforworkandstudy@gmail.com>
2025-11-18 16:51:56 +0530

    some-fixes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
+"use client";
 export default function Hero() {
   return (
     <section className="relative flex flex-col justify-center items-center px-4 sm:px-8 md:px-16 h-screen text-xl sm:text-2xl md:text-3xl font-light text-black">
-
       <nav className="flex flex-row justify-between absolute p-3 top-4 sm:top-6 md:top-8 left-4 right-4 sm:left-8 sm:right-8 md:left-16 md:right-16 bg-phantasia-blue h-12 sm:h-14 md:h-15 rounded-xl sm:rounded-2xl">
         <img src={"/fs.svg"} />
         <img src={"/menu.svg"} />
@@ -13,15 +13,16 @@ export default function Hero() {
           <div>2026</div>
         </div>
         <img src={"/phantasia.svg"} className="w-full" alt="Phantaisa Logo" />
-        <div>
-          Hackathon
-        </div>
+        <div>Hackathon</div>
       </div>
 
-
-      <div className="absolute bottom-12 sm:bottom-16 md:bottom-24 w-fit px-12 sm:px-20 md:px-32 bg-phantasia-blue text-2xl sm:text-3xl md:text-4xl rounded-xl sm:rounded-2xl p-2 font-medium">
+      <a
+        href="https://www.phantasia.tech/"
+        target="blank"
+        className="absolute bottom-12 sm:bottom-16 md:bottom-24 w-fit px-12 sm:px-20 md:px-32 bg-phantasia-blue text-2xl sm:text-3xl md:text-4xl rounded-xl sm:rounded-2xl p-2 font-medium cursor-pointer inline-block"
+      >
         Register Now
-      </div>
+      </a>
     </section>
-  )
+  );
 }
